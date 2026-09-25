12b0f631d22405ccc5a31b59db1a7d3cc307f654
ShaviRajapaksha <shavindurajapaksha@gmail.com>
2026-09-25 21:34:55 +0530

    feat: Add .gitignore file and update assembly informational version

--- AuthApi/AuthApi/obj/Debug/net8.0/AuthApi.AssemblyInfo.cs
+++ AuthApi/AuthApi/obj/Debug/net8.0/AuthApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1439063ee109ee1f105e1299cb58b08acc445cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d36441d1bc8f67e18f0b96c9cacfdc843aaea1b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
